24a662c103d0dee248d25ffc90236d6deffa477e
lili327 <ytang4@binghamton.edu>
2014-02-25 22:52:59 -0500

    Fixed indentation issues

--- confclient.c
+++ confclient.c
@@ -25,54 +25,54 @@ int hooktoserver(const char *servhost, const char* servport);
 /*--------------------------------------------------------------------*/
 
 void build_set(int *sock, fd_set *readfds){
-	FD_ZERO(readfds);
-	FD_SET(*sock, readfds);
-	FD_SET(STDIN_FILENO, readfds);
+		FD_ZERO(readfds);
+		FD_SET(*sock, readfds);
+		FD_SET(STDIN_FILENO, readfds);
 }		
 
 void service_server(int *sock){
-	char *msg = recvtext(*sock);
-	if (!msg) { 
-		fprintf(stderr, "error: server died\n");
-		exit(1);
-	}
-	printf(">>> %s", msg);
-	free(msg);
+		char *msg = recvtext(*sock);
+		if (!msg) { 
+				fprintf(stderr, "error: server died\n");
+				exit(1);
+		}
+		printf(">>> %s", msg);
+		free(msg);
 }
 
 void service_user(int *sock){
-	char msg[MAXMSGLEN];
-	if (!fgets(msg, sizeof msg, stdin)) {
-		close(*sock);
-	}
-	sendtext(*sock, msg);
+		char msg[MAXMSGLEN];
+		if (!fgets(msg, sizeof msg, stdin)) {
+				close(*sock);
+		}
+		sendtext(*sock, msg);
 }
 
 int main(int argc, char *argv[]) {
-	/* check usage */
-  if (argc != 3) {
-    fprintf(stderr, "usage : %s <servhost> <servport>\n", argv[0]);
-    exit(1);
-  }
+		/* check usage */
+		if (argc != 3) {
+				fprintf(stderr, "usage : %s <servhost> <servport>\n", argv[0]);
+				exit(1);
+		}
 
-  /* get hooked on to the server */
-  int sock = hooktoserver(argv[1], argv[2]);
-  if (sock == -1) exit(1);
-	fd_set readfds;
+		/* get hooked on to the server */
+		int sock = hooktoserver(argv[1], argv[2]);
+		if (sock == -1) exit(1);
+		fd_set readfds;
 
-  while (1) {
-		build_set(&sock, &readfds);
-		if (select(sock + 1, &readfds, NULL, NULL, NULL) < 0 ){
-			perror("select error");
-			exit(1);
+		while (1) {
+				build_set(&sock, &readfds);
+				if (select(sock + 1, &readfds, NULL, NULL, NULL) < 0 ){
+						perror("select error");
+						exit(1);
+				}
+				if (FD_ISSET(sock, &readfds)){
+						service_server(&sock);
+				}
+				if (FD_ISSET(STDIN_FILENO, &readfds)){
+						service_user(&sock);
+				}
 		}
-		if (FD_ISSET(sock, &readfds)){
-			service_server(&sock);
-		}
-		if (FD_ISSET(STDIN_FILENO, &readfds)){
-			service_user(&sock);
-		}
-  }
 
 }
 /*--------------------------------------------------------------------*/
