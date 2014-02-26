adefb032d6ee0c2ddcf8547c12cf5a4b3e18340b
Ryan Haining <rhainin1@binghamton.edu>
2014-02-26 12:40:01 -0500

    includes own header

--- confutils.c
+++ confutils.c
@@ -1,6 +1,8 @@
 /*--------------------------------------------------------------------*/
 /* functions to connect clients and server */
 
+#include "confutils.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
