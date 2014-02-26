6714b4e044cb8d6ecae82d61b2524c534a38bcc0
Ryan Haining <rhainin1@binghamton.edu>
2014-02-26 12:37:36 -0500

    Creates header file to eliminate redundancy
    confclient and confserver both have declarations for the functions in
    confutils.  these declarations should be centralized, and more obviouly
    assocated with confutils.c

---
+++ confutils.h
@@ -0,0 +1,9 @@
+#ifndef CONFUTILS__H__
+#define CONFUTILS__H__
+
+char *recvtext(int sd);
+int sendtext(int sd, char *msg);
+int start_server();
+int hook_to_server(const char *servhost, const char* servport);
+
+#endif // #ifndef CONFUTILS__H__
